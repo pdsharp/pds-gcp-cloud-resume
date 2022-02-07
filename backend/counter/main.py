2a7a4fd40d1f59e0b2bbbc5448f44145517c9d37
Patrick Sharp <pdsharp@gmail.com>
2022-02-06 18:02:34 -0800

    headers fix

--- backend/counter/main.py
+++ backend/counter/main.py
@@ -5,9 +5,13 @@ db = firestore.Client()
 
 def visitcount(request):
     counter = db.collection(u'counter').document(u'counter')
-    newcount = int(counter.get().to_dict()['count']) + 1
-    counter.set({'count': newcount})
     headers = {
         'Access-Control-Allow-Origin': '*'
     }
+    try:
+        newcount = int(counter.get().to_dict()['count']) + 1
+        counter.set({'count': newcount})
+    except (TypeError):
+        newcount = 1
+        counter.set({'count': newcount})
     return jsonify(newcount), 200, headers
