ba3a41ee9c1f44bf8561f668f308f2b1680dd057
Patrick Sharp <pdsharp@gmail.com>
2022-02-11 12:13:10 -0800

    update

--- backend/counter/main.py
+++ backend/counter/main.py
@@ -1,17 +1,18 @@
 from google.cloud import firestore
-from flask import jsonify
+
 
 db = firestore.Client()
 
-def visitcount(request):
+def visitcount(request, db):
     counter = db.collection(u'counter').document(u'counter')
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
     try:
         newcount = int(counter.get().to_dict()['count']) + 1
         counter.set({'count': newcount})
     except (TypeError):
         newcount = 1
         counter.set({'count': newcount})
-    return jsonify(newcount), 200, headers
+    return {
+	    'body': newcount,
+	    'statusCode': 200,
+	    'headers': {'Access-Control-Allow-Origin': '*'}
+    }
