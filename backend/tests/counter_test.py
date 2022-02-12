b7299cdf348b79e2f39335cf593e6dd5d92e38b6
Patrick Sharp <pdsharp@gmail.com>
2022-02-11 19:47:48 -0800

    tests

--- backend/tests/counter_test.py
+++ backend/tests/counter_test.py
@@ -4,6 +4,11 @@ from unittest.mock import patch
 
 @patch('counter.main.db', fs.MockFirestore())
 def test_visitcount():
+    db = fs.MockFirestore()
+    counter = db.collection(u'counter').document(u'counter')
+    counter.set({'count': 42})
+    newcount = int(counter.get().to_dict()['count'])
     response = visitcount(None)
+    assert newcount == 42
     assert response[0] == '1'
     assert response[1] == 200
