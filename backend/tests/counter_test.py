ecf48d1fbadf98b71daf8e2ead438cca68f8fa9c
Patrick Sharp <pdsharp@gmail.com>
2022-02-11 21:17:33 -0800

    separate tests

--- backend/tests/counter_test.py
+++ backend/tests/counter_test.py
@@ -2,13 +2,16 @@ import mockfirestore as fs
 from counter.main import visitcount
 from unittest.mock import patch
 
-@patch('counter.main.db', fs.MockFirestore())
-def test_visitcount():
+
+def test_get_db():
     db = fs.MockFirestore()
     counter = db.collection(u'counter').document(u'counter')
     counter.set({'count': 42})
     newcount = int(counter.get().to_dict()['count'])
-    response = visitcount(None)
     assert newcount == 42
+
+@patch('counter.main.db', fs.MockFirestore())
+def test_insert_db():
+    response = visitcount(None)
     assert response[0] == '1'
     assert response[1] == 200
