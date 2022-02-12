24807b67eabd0b68410427c9805f2efd9f7d48b6
Patrick Sharp <pdsharp@gmail.com>
2022-02-11 18:50:35 -0800

    tests

--- backend/tests/counter_test.py
+++ backend/tests/counter_test.py
@@ -2,14 +2,8 @@ import mockfirestore as fs
 from counter.main import visitcount
 from unittest.mock import patch
 
-
-
 @patch('counter.main.db', fs.MockFirestore())
 def test_visitcount():
-#    db = fs.MockFirestore()
-#    counter = db.collection(u'counter').document(u'counter')
-#    counter.set({'count': 3})
     response = visitcount(None)
-    print(response)
     assert response[0] == '1'
     assert response[1] == 200
