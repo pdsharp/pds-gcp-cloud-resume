de7433675fb354fb713be611eaabe417190f8c4a
Patrick Sharp <pdsharp@gmail.com>
2022-02-18 17:41:42 -0800

    js fixes

--- frontend/js/counter.js
+++ frontend/js/counter.js
@@ -1,19 +1,6 @@
-const functionApi = 'https://us-west1-pds-cloud-resume.cloudfunctions.net/counter'
-
-const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApi)
-    .then(response => {
-        return response.text()
-    })
-    .then(response => {
-        console.log("Website called function API.",response);
-        count = response;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
-        console.log(error);
-      });
-    return count;
+const getCount = () => {
+  fetch("https://us-west1-pds-cloud-resume.cloudfunctions.net/counter")
+  .then(response => response.text())
+  .then(response => document.getElementById('counter').innerText = response)
 }
-
-getVisitCount();
+getCount();
